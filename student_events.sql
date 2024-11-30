a720b46057396fe32b85627de92f9d0c88ae0026
Vlad Valeros <144913325+vladValeros@users.noreply.github.com>
2024-11-30 16:01:19 +0800

    Update student_events.sql

--- student_events.sql
+++ student_events.sql
@@ -1,1 +1,191 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2024 at 08:59 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_events`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `student_number` varchar(20) NOT NULL,
+  `student_name` varchar(100) NOT NULL,
+  `time_in` time NOT NULL,
+  `time_out` time NOT NULL,
+  `attendance_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `event_id`, `student_number`, `student_name`, `time_in`, `time_out`, `attendance_date`) VALUES
+(1, 2, '202301754', 'Eros Denz Etac', '16:43:00', '13:49:00', '2024-11-30 05:43:24'),
+(2, 1, '202301224', 'Jasmin Majid', '13:51:00', '13:50:00', '2024-11-30 05:47:19'),
+(3, 1, '202301754', 'Eros Denz Etac', '13:56:00', '13:57:00', '2024-11-30 05:53:08'),
+(4, 3, '12345', 'Jake', '19:42:00', '15:49:00', '2024-11-30 07:43:07'),
+(5, 3, '202302172', 'Paul Seupon', '21:43:00', '15:47:00', '2024-11-30 07:44:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `event_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `title`, `description`, `event_date`) VALUES
+(1, 'Event1-1', 'Lorem Ipsum', '2024-11-30'),
+(2, 'Event2', 'Lorem Ipsum 2', '2024-11-06'),
+(3, 'Event3', 'Lorem Ipsum', '2024-11-13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `student_number` varchar(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `course` varchar(100) NOT NULL,
+  `year_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `student_number`, `name`, `course`, `year_level`) VALUES
+(1, '202300727', 'Ethan Wayne Cassion', 'Computer Science', 2),
+(2, '202301754', 'Eros Denz Etac', 'Computer Science', 2),
+(3, '202301224', 'Jasmin Majid', 'Computer Science', 2),
+(4, '202302172', 'Paul Seupon', 'Computer Science', 2),
+(8, '12345', 'Jake', 'IT', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','officer') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', '$2y$10$JQq8S0KIrdPAW9SxCK6S7uzYACIVk3fbswfJbeSjYWiQvUO63JXvS', 'admin'),
+(2, 'paul', '$2y$10$GyAW7h0f/Vm1Xt0U3Uij9.rG6WjHntNLtHPTUWLY812o2AVBUZsie', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `event_id` (`event_id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_number` (`student_number`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
